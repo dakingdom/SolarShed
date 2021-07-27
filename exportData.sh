7fbe380a118b31cf139098f2b10e3868d004940f
Andy King <andyeking@gmail.com>
2021-07-26 21:10:03 -0700

    fixed some typos and streamline files

--- exportData.sh
+++ exportData.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 
-# This script converts the plain ASCII data output by getTracerData.py into a format
-# that can be used by Grafana. The new file is written to the /ramdisk folder
-# It converts the file /ramdisk/solarData.txt to /ramdisk/solarData.txt.prom
-# 
-# Here is an example wrapper script that brings it altogether, loading data into grafana indefinitely.
-
-#!/bin/bash
-#
-echo starting Epever LifePO4 serial data collection
-while : ; do
-    /home/solar/getTracerData.py > /ramdisk/solarData.txt.$$
-    sleep 1
-    date >> /ramdisk/solarData.txt.$$
-    mv /ramdisk/solarData.txt.$$ /ramdisk/solarData.txt
-    /home/solar/exportData.sh
-    sleep 4
-done
-
-# Create the above script called runGetSolarData.sh, make it executable with '# chmod +x runGetSolarData.sh'
-# Then run it as root ./runGetSolarData.sh 
-# look for the new file being created /ramdisk/solarData.txt.prom and updated every 4 seconds
-# node_exporter will automatically load this data into Prometheus for you.
-
-# In Grafana, the Prometheus data can be seen under the label 'AB_SolarStats'
-# Ensure Prometheus and node_exporter are working correctly.
-
-# example test commands to create load on your Pi server for testing with node_exporter
-# create CPU & LoadAvg activity with # while :; do :; done
-# create DISK load with # find /usr -exec grep XXX {} \;
-# create Network load with # ping -f 8.8.8.8
-
 # Add a description string of your solar installation
-unitName="1440w-3x4-120W LifePO4-8s4p 24v-640a/h 15.4kW/h"
+#unitName="1440w-3x4-120W LifePO4-8s4p 24v-640a/h 15.4kW/h"
+unitName="Cabin Solar"
 
 # The source text file containing the output from the getTracerData.py script
 dataFile="/ramdisk/solarData.txt"
@@ -54,28 +24,28 @@ chargeStatVal=`cat $dataFile | \grep chargeStatus | cut -f2 -d" "`
 chargeStatStr=`cat $dataFile | \grep chargeStatus | cut -f3 -d" "`
 sysStatus=`cat $dataFile | \grep Battery\ is | cut -f3 -d" "`
 
-:>/ramdisk/$dataFile.prom.$$
+:>$dataFile.prom.$$
 
  # numeric values
-printf "AB_SolarStats{mode=\"batVolts\"} $batVolts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"batwatts\"} $batwatts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"batSOC\"} $batSOC\n"     >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"pvWatts\"} $pvWatts\n"   >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"pvVolts\"} $pvVolts\n"   >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"loadWatts\"} $loadWatts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"loadCurr\"} $loadCurr\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"loadVolts\"} $loadVolts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"batTemp\"} $batTemp\n"   >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"devTemp\"} $devTemp\n"   >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"genWatts\"} $genWatts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"conWatts\"} $conWatts\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{mode=\"chargeStatVal\"} $chargeStatVal\n" >> /ramdisk/$dataFile.prom.$$
+printf "AB_SolarStats{mode=\"batVolts\"} $batVolts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"batwatts\"} $batwatts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"batSOC\"} $batSOC\n"     >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"pvWatts\"} $pvWatts\n"   >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"pvVolts\"} $pvVolts\n"   >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"loadWatts\"} $loadWatts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"loadCurr\"} $loadCurr\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"loadVolts\"} $loadVolts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"batTemp\"} $batTemp\n"   >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"devTemp\"} $devTemp\n"   >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"genWatts\"} $genWatts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"conWatts\"} $conWatts\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{mode=\"chargeStatVal\"} $chargeStatVal\n" >> $dataFile.prom.$$
 
 # text values
-printf "AB_SolarStats{myVar=\"chargeStatStr\",myStr=\"$chargeStatStr\"} $chargeStatVal\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{myVar=\"sysStatus\",myStr=\"$sysStatus\"} $chargeStatVal\n" >> /ramdisk/$dataFile.prom.$$
-printf "AB_SolarStats{myVar=\"unitName\",myStr=\"$unitName\"} 0\n" >> /ramdisk/$dataFile.prom.$$
+printf "AB_SolarStats{myVar=\"chargeStatStr\",myStr=\"$chargeStatStr\"} $chargeStatVal\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{myVar=\"sysStatus\",myStr=\"$sysStatus\"} $chargeStatVal\n" >> $dataFile.prom.$$
+printf "AB_SolarStats{myVar=\"unitName\",myStr=\"$unitName\"} 0\n" >> $dataFile.prom.$$
 
-`mv /ramdisk/$dataFile.prom.$$ /ramdisk/$dataFile.prom`
+`mv $dataFile.prom.$$ $dataFile.prom`
 
 # End
---
+++ runGetSolarData.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script converts the plain ASCII data output by getTracerData.py into a format
+# that can be used by Grafana. The new file is written to the /ramdisk folder
+# It converts the file /ramdisk/solarData.txt to /ramdisk/solarData.txt.prom
+# 
+# Here is an example wrapper script that brings it altogether, loading data into grafana indefinitely.
+
+#!/bin/bash
+#
+echo starting Epever LifePO4 serial data collection
+while : ; do
+    /home/solar/getTracerData.py > /ramdisk/solarData.txt.$$
+    sleep 1
+    date >> /ramdisk/solarData.txt.$$
+    mv /ramdisk/solarData.txt.$$ /ramdisk/solarData.txt
+    /home/solar/exportData.sh
+    sleep 4
+done
+
+# Create the above script called runGetSolarData.sh, make it executable with '# chmod +x runGetSolarData.sh'
+# Then run it as root ./runGetSolarData.sh 
+# look for the new file being created /ramdisk/solarData.txt.prom and updated every 4 seconds
+# node_exporter will automatically load this data into Prometheus for you.
+
+# In Grafana, the Prometheus data can be seen under the label 'AB_SolarStats'
+# Ensure Prometheus and node_exporter are working correctly.
+
+# example test commands to create load on your Pi server for testing with node_exporter
+# create CPU & LoadAvg activity with # while :; do :; done
+# create DISK load with # find /usr -exec grep XXX {} \;
+# create Network load with # ping -f 8.8.8.8
